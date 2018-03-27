e365740f1c39951156e5c03c8ea08630b430dad9
Andrew Locke <alocke12992@gmail.com>
2018-03-27 16:45:37 -0600

    adds settings
    Reviewers: spencer.richards
    
    Reviewed By: spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D1436

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321225533) do
+ActiveRecord::Schema.define(version: 20180322224923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,6 @@ ActiveRecord::Schema.define(version: 20180321225533) do
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_discords_on_deleted_at"
     t.index ["user_id"], name: "index_discords_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "url"
-    t.string "kind"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "reward_id"
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_posts_on_deleted_at"
   end
 
   create_table "rewards", force: :cascade do |t|
@@ -140,7 +130,7 @@ ActiveRecord::Schema.define(version: 20180321225533) do
     t.boolean "translator", default: false
     t.boolean "is_influencer", default: false
     t.jsonb "api_token"
-    t.boolean "is_dev", default: false
+    t.jsonb "github"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -149,7 +139,6 @@ ActiveRecord::Schema.define(version: 20180321225533) do
   end
 
   add_foreign_key "discords", "users"
-  add_foreign_key "rewards", "posts"
   add_foreign_key "rewards", "submissions"
   add_foreign_key "rewards", "users"
   add_foreign_key "submissions", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 Setting.create()
 
 puts 'Setting created with defaults'
->>>>>>> add settings hoc
