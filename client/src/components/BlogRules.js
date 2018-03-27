1ac79ddf02cc4508369153488f50597243e94a4b
Tyson Au <au.tyson@gmail.com>
2018-03-27 14:26:00 -0600

    Fixing console errors
    Reviewers: spencer.richards, #staff
    
    Reviewed By: spencer.richards, #staff
    
    Differential Revision: http://phabricator.devpointstudios.com/D1427

--- client/src/components/BlogRules.js
+++ client/src/components/BlogRules.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import { Header, Segment, List } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 
 class BlogRules extends Component {
   render() {
     return (
       <Segment>
-        <p><strong><span style={{fontSize: 16}}>Shares for Blog and Reddit posts:</span></strong></p>
-        <p>*Posts must include the statement: &ldquo;I am receiving DCO for growing the community.&rdquo;</p>
-        <p><em>250 shares for 100-500 followers</em></p>
-        <p><em>500 shares for 501-1000 followers</em></p>
-        <p><em>750 shares for 1001-10,000 followers</em></p>
-        <p><em>1000 shares for 10,001-50,000 followers</em></p>
-        <p><em>2500 shares for 50,001-100,000 followers</em></p>
-        <p><em>5000 shares for 100,001+ followers</em></p>
+        <p>
+          <strong>
+            <span style={{ fontSize: 16 }}>
+              Shares for Blog and Reddit posts:
+            </span>
+          </strong>
+        </p>
+        <p>
+          *Posts must include the statement: &ldquo;I
+          am receiving DCO for growing the
+          community.&rdquo;
+        </p>
+        <p>
+          <em>250 shares for 100-500 followers</em>
+        </p>
+        <p>
+          <em>500 shares for 501-1000 followers</em>
+        </p>
+        <p>
+          <em>750 shares for 1001-10,000 followers</em>
+        </p>
+        <p>
+          <em>
+            1000 shares for 10,001-50,000 followers
+          </em>
+        </p>
+        <p>
+          <em>
+            2500 shares for 50,001-100,000 followers
+          </em>
+        </p>
+        <p>
+          <em>5000 shares for 100,001+ followers</em>
+        </p>
       </Segment>
     );
   }
--- client/src/components/SocialMediaRules.js
+++ client/src/components/SocialMediaRules.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { Header, Segment, List } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 
 class SocialMediaRules extends Component {
   render() {
     return (
       <Segment>
-        <p><strong><em>Social Media: Facebook, Linkedin, or Twitter</em></strong></p>
-        <p><em>&nbsp;20 shares: Follow/Subscribe/Like Deconet on FB, Twitter, LinkedIn, and Reddit</em></p>
+        <p>
+          <strong>
+            <em>
+              Social Media: Facebook, Linkedin, or
+              Twitter
+            </em>
+          </strong>
+        </p>
+        <p>
+          <em>
+            &nbsp;20 shares: Follow/Subscribe/Like
+            Deconet on FB, Twitter, LinkedIn, and
+            Reddit
+          </em>
+        </p>
       </Segment>
     );
   }
