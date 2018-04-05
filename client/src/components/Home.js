039f8a690f4624206ed3e6d3327f8de6685e205a
Tyson Au <au.tyson@gmail.com>
2018-04-05 17:06:35 -0600

    Change home component to render chosen providers
    Reviewers: spencer.richards
    
    Reviewed By: spencer.richards
    
    Differential Revision: http://phabricator.devpointstudios.com/D1567

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Dashboard from './Dashboard';
 import Facebook from './Facebook';
 import LinkedIn from './LinkedIn';
@@ -9,33 +9,130 @@ import Social from './Social';
 import Translation from './Translation';
 import Twitter from './Twitter';
 import VideoContest from './VideoContest';
-import {connect} from 'react-redux';
-import {Container, Grid, Menu, Responsive} from 'semantic-ui-react';
-import {getRewards} from '../actions/rewards';
-import {Link, Route, Switch, } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+  Container,
+  Grid,
+  Menu,
+  Responsive,
+} from 'semantic-ui-react';
+import { getRewards } from '../actions/rewards';
+import { Link, Route, Switch } from 'react-router-dom';
 
 class Home extends React.Component {
+  state = { currentRoutes: [] };
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.dispatch(getRewards());
+    this.toggleRoutes();
+  }
+
+  setActive = (routes) => {
+    const { currentRoutes } = this.state;
+    const activeRoutes = [];
+    routes.map(
+      (route) =>
+        route.active === true && activeRoutes.push(route),
+    );
+    this.setState({ currentRoutes: activeRoutes });
+  };
+
+  toggleRoutes = () => {
+    const {
+      facebook,
+      twitter,
+      linkedin,
+      reddit,
+    } = this.props;
+
+    const routes = [
+      {
+        name: 'Dashboard',
+        path: '/',
+        exact: true,
+        active: true,
+        main: () => <Dashboard />,
+      },
+      {
+        name: 'Influencer',
+        path: '/influencer',
+        active: true,
+        main: () => (
+          <Social
+            service="influencer"
+            submissions={true}
+          />
+        ),
+      },
+      {
+        name: 'Translation',
+        path: '/translation',
+        active: true,
+        main: () => <Translation />,
+      },
+      {
+        name: 'Video Contest',
+        path: '/contest',
+        active: true,
+        main: () => <VideoContest />,
+      },
+      {
+        name: 'Rules and Overview',
+        path: '/rules',
+        active: true,
+        main: () => <Rules />,
+      },
+      {
+        name: 'Facebook',
+        path: '/facebook',
+        active: facebook,
+        main: () => <Facebook service="facebook" />,
+      },
+      {
+        name: 'Twitter',
+        path: '/twitter',
+        active: twitter,
+        main: () => (
+          <Twitter service="twitter" submissions={true} />
+        ),
+      },
+      {
+        name: 'LinkedIn',
+        path: '/linkedin',
+        active: linkedin,
+        main: () => (
+          <LinkedIn
+            service="linkedin"
+            submissions={true}
+          />
+        ),
+      },
+      {
+        name: 'Reddit',
+        path: '/reddit',
+        active: reddit,
+        main: () => (
+          <Reddit service="reddit" submissions={true} />
+        ),
+      },
+    ];
+    this.setActive(routes);
   };
 
   render() {
-    const {pathname} = this.props.location;
+    const { pathname } = this.props.location;
+    const { currentRoutes } = this.state;
 
     return (
       <Container>
         <Grid stackable>
           <Grid.Row>
-            <Grid.Column mobile={16} tablet={16} computer={5}>
-              <Responsive
-                as={Menu}
-                minWidth={992}
-                pointing
-                vertical
-                size='large'
-              >
-                {routes.map((route, index) => (
+            <Grid.Column
+              mobile={16}
+              tablet={16}
+              computer={5}>
+              <Menu pointing vertical size="large">
+                {currentRoutes.map((route, index) => (
                   <Menu.Item
                     key={index}
                     as={Link}
@@ -44,11 +141,14 @@ class Home extends React.Component {
                     active={pathname === route.path}
                   />
                 ))}
-              </Responsive>
+              </Menu>
             </Grid.Column>
-            <Grid.Column mobile={16} tablet={16} computer={11}>
+            <Grid.Column
+              mobile={16}
+              tablet={16}
+              computer={11}>
               <Switch>
-                {routes.map((route, index) => (
+                {currentRoutes.map((route, index) => (
                   <Route
                     key={index}
                     path={route.path}
@@ -62,57 +162,22 @@ class Home extends React.Component {
           </Grid.Row>
         </Grid>
       </Container>
-    )
+    );
   }
+}
+
+const mapStateToProps = (state) => {
+  const {
+    facebook,
+    twitter,
+    linkedin,
+    reddit,
+  } = state.settings;
+  return {
+    facebook,
+    twitter,
+    linkedin,
+    reddit,
+  };
 };
-
-const routes = [
-  {
-    name: 'Dashboard',
-    path: '/',
-    exact: true,
-    main: () => <Dashboard />
-  },
-  {
-    name: 'Facebook',
-    path: '/facebook',
-    main: () => <Facebook service="facebook" />
-  },
-  {
-    name: 'Twitter',
-    path: '/twitter',
-    main: () => <Twitter service="twitter" submissions={true} />
-  },
-  {
-    name: 'LinkedIn',
-    path: '/linkedin',
-    main: () => <LinkedIn service="linkedin" submissions={true} />
-  },
-  {
-    name: 'Reddit',
-    path: '/reddit',
-    main: () => <Reddit service="reddit" submissions={true} />
-  },
-  {
-    name: 'Influencer',
-    path: '/influencer',
-    main: () => <Social service="influencer" submissions={true} />
-  },
-  {
-    name: 'Translation',
-    path: '/translation',
-    main: () => <Translation />
-  },
-  {
-    name: 'Video Contest',
-    path: '/contest',
-    main: () => <VideoContest />
-  },
-  {
-    name: 'Rules and Overview',
-    path: '/rules',
-    main: () => <Rules />
-  }
-];
-
-export default connect()(Home);
+export default connect(mapStateToProps)(Home);
